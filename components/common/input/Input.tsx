1e74c6d537b23a342644d1710d18a750d081beb9
yeonhan <yeonhanlab@gmail.com>
2026-07-25 22:14:45 +0900

    Input 컴포넌트 작성

--- components/common/input/Input.tsx
+++ components/common/input/Input.tsx
@@ -1,5 +1,5 @@
 import { TextInput, TextInputProps } from "react-native";
-import { StyleSizeType } from "@/types/style";
+import { INPUT_SIZE_STYLE, StyleSizeType } from "@/types/style";
 import { twMerge } from "tailwind-merge";
 
 interface InputProps extends TextInputProps {
@@ -12,10 +12,18 @@ function Input({
     size = "medium",
     className,
     placeholderClassName,
-    ...props}: InputProps) {
+    ...props
+}: InputProps) {
     return (
-        <TextInput className={twMerge(
-            "w-full bg-background-paper rounded-[18px]"
+        <TextInput
+            className={twMerge(
+                "w-full bg-background-paper rounded-[18px] border border-divider text-text-default",
+                INPUT_SIZE_STYLE[size],
+                hasError ? "border-error-main" : "border-primary-main focus:border-primary-main",
+                className,
+            )}
+            placeholderClassName={twMerge("text-text-secondary", placeholderClassName)}
+            {...props}></TextInput>
     );
 }
 
