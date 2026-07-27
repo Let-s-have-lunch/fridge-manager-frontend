d9591f88438b935e72acf1196ee9caf7a0795bc6
yeonhan <yeonhanlab@gmail.com>
2026-07-27 16:51:30 +0900

    MainHeader 작성중

--- types/style.ts
+++ types/style.ts
@@ -1,6 +1,6 @@
 export type StyleColorType = "primary" | "secondary" | "success" | "error" | "warning" | "info" | "text";
 export type StyleVariantType = "contained-square" | "contained-circle";
-export type StyleSizeType =  "small" | "medium" | "large";
+export type StyleSizeType = "small" | "medium" | "large";
 
 export const INPUT_SIZE_STYLE = {
     mini: "text-xs px-1 py-1",
